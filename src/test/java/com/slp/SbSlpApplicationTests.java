8b58659755b459157273bd61cbd78ffc2f8acfa5
Jenish Immanuel Raj J <144326843+Jenishrz@users.noreply.github.com>
2024-01-17 17:36:18 +0530

    Update SbSlpApplicationTests.java

--- src/test/java/com/slp/SbSlpApplicationTests.java
+++ src/test/java/com/slp/SbSlpApplicationTests.java
@@ -1,4 +1,4 @@
-package com.slp;
+/*package com.slp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class SbSlpApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}*/
